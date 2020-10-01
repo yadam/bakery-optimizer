148c4f0ef1604e4fe0ddf5be92952ddd1ecc21e7
Adam Bantly <24380+yadam@users.noreply.github.com>
2020-10-01 15:35:58 -0700

    Initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: [
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+    "prettier",
+    "prettier/react",
+  ],
+  plugins: ["jest"],
+};
